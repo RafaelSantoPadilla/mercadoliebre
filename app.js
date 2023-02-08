37248e1ae0b11c9789dde28641ad087209a7efa9
unknown <santopd1997@gmail.com>
2023-02-07 21:47:40 -0500

    agregando cambios

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const { join } = require('path');
 const app = express();
 const path = require('path');
 app.use(express.static('public'));
-const port = 3100;
+
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './views/home.html'));
@@ -18,7 +18,7 @@ app.get('/login', (req, res) => {
 });
 
 
-
+const port = process.env.PORT || 3100;
 app.listen(port, () => {
-    console.log('Server started on http://localhost:3100');
+    console.log(`Server started on http://localhost:${port}`);
 }); 
